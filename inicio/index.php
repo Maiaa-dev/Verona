835fa318cc429adfefdefe83181fcf54719fadec
Aline Maia (✿◡‿◡) <alimaaia.dev@gmail.com>
2025-11-23 18:32:31 -0300

    Correção dos slides

--- inicio/index.php
+++ inicio/index.php
@@ -63,11 +63,11 @@
                 <img src="../imagens/slide3.png" class="d-block w-100" alt="...">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#meuCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#meuCarousel" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
